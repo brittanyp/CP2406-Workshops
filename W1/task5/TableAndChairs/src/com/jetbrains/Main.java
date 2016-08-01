ea38c17a3664441684cd4b910ae6f1482413e4ad
brittanyp <brittany.peever@my.jcu.edu.au>
2016-08-01 17:35:57 +1000

    Workshop1
    Debugging tasks, hello world task

---
+++ W1/task5/TableAndChairs/src/com/jetbrains/Main.java
@@ -0,0 +1,13 @@
+package com.jetbrains;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println
+                ("X                         X\n" +
+                 "X                         X\n" +
+                 "X        XXXXXXXX         X\n" +
+                 "XXXXXXX  X      X   XXXXXXX\n" +
+                 "X     X  X      X   X     X\n");
+    }
+}
