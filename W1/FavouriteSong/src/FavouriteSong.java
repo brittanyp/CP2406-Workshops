5094e623eb68ea5debb2263c2a4f1026a4be9fcf
brittanyp <brittany.peever@my.jcu.edu.au>
2016-08-01 17:47:17 +1000

    Workshop1.1
    Corrected files

---
+++ W1/FavouriteSong/src/FavouriteSong.java
@@ -0,0 +1,12 @@
+/**
+ * Created by jc319715 on 1/08/16.
+ */
+public class FavouriteSong {
+
+    public static void main(String[] args) {
+        System.out.println("Vamos a la playa oh oh oh oh\n" +
+                "Vamos a la playa oh oh oh oh\n" +
+                "Vamos a la playa oh oh oh oh\n" +
+                "Vamos a la playa oh oh oh oh\n" );
+    }
+}
---
+++ W1/FavouriteSong/src/TablenChairs.java
@@ -0,0 +1,14 @@
+/**
+ * Created by jc319715 on 1/08/16.
+ */
+public class TablenChairs {
+
+    public static void main(String[] args) {
+        System.out.println
+                ("X                         X\n" +
+                        "X                         X\n" +
+                        "X        XXXXXXXX         X\n" +
+                        "XXXXXXX  X      X   XXXXXXX\n" +
+                        "X     X  X      X   X     X\n");
+    }
+}
